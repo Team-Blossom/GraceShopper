c3f38738d766cfafe983ec6d13b937744eb59d91
Sara Brea <Sara@MyMacs-MBP.attlocal.net>
2020-06-18 15:14:01 -0500

    seperated product/products redux deleted create category added to reducers

--- client/store/products.js
+++ client/store/products.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
-//const GET_PRODUCT = 'GET_PRODUCT'
-const GET_PRODUCTS = 'GET_PRODUCTS'
-const GET_PRODUCT = 'GET_PRODUCT'
+const SET_PRODUCTS = 'SET_PRODUCTS'
 
-//const getProduct = id =>({type:GET_PRODUCT, id})
-const getProducts = products => ({type: GET_PRODUCTS, products})
-const getProduct = product => ({type: GET_PRODUCT, product})
+const getProducts = products => ({type: SET_PRODUCTS, products})
+
 //add product
 //delete product
 
@@ -17,20 +14,11 @@ export const fetchProducts = () => {
   }
 }
 
-export const fetchProduct = id => {
-  return async dispatch => {
-    const product = await axios.get(`/api/products/${id}`)
-
-    dispatch(getProduct(product.data))
-  }
-}
-const initialState = {product: {}, products: []}
-export default function(state = initialState, action) {
+export default function(state = [], action) {
   switch (action.type) {
-    case GET_PRODUCTS:
-      return {...initialState, products: action.products}
-    case GET_PRODUCT:
-      return {...initialState, product: action.product}
+    case SET_PRODUCTS:
+      return action.products
+
     default:
       return state
   }
--- client/store/products.spec.js
+++ client/store/products.spec.js
@@ -1,7 +1,8 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {fetchProducts, fetchProduct} from './products'
+import {fetchProducts} from './products'
+import {fetchProduct} from './product'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -28,23 +29,23 @@ describe('thunk creators', () => {
   })
 
   describe('fetchProducts', () => {
-    it('eventually dispatches the GET_PRODUCTS action', async () => {
+    it('eventually dispatches the SET_PRODUCTS action', async () => {
       const products = [{name: 'henry'}, {name: 'sara'}]
       mockAxios.onGet('/api/products').replyOnce(200, products)
       await store.dispatch(fetchProducts())
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GET_PRODUCTS')
+      expect(actions[0].type).to.be.equal('SET_PRODUCTS')
       expect(actions[0].products).to.be.deep.equal(products)
     })
   })
 
   describe('fetchProduct', () => {
-    it('eventually dispatches the GET_PRODUCT action', async () => {
+    it('eventually dispatches the SET_PRODUCT action', async () => {
       const product = {name: 'product', id: 1}
       mockAxios.onGet('/api/products/1').replyOnce(200, product)
       await store.dispatch(fetchProduct(1))
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GET_PRODUCT')
+      expect(actions[0].type).to.be.equal('SET_PRODUCT')
       expect(actions[0].product).to.be.deep.equal(product)
     })
   })
