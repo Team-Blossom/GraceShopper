4d10683339c6264ec26302cc044a9166e9c064da
Niko Sward <nicholascsward@gmail.com>
2020-06-18 16:59:18 -0500

    integrated Sign Up and Register

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Register, UserHome} from './components'
 import {me} from './store'
 
 /**
@@ -20,7 +20,8 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/signup" component={Register} />
+
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
