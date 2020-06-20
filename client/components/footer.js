d44e601fa60c88724e0c63ddbcee4e0c825d3b23
Niko Sward <nicholascsward@gmail.com>
2020-06-19 19:08:22 -0500

    footer finished

---
+++ client/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => (
+  <footer>
+    <p>
+      UTONIUM <span style={{color: '#F77F00'}}>&copy;</span> 2020
+    </p>
+    <p>Low Skrull Prices. High Quality Standards. Forever.</p>
+    <a href="">Admin Login</a>
+  </footer>
+)
