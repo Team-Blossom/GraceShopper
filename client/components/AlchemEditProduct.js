ca3819cae0fe676aac65a03c7b44db75284a4b51
Sara Brea <Sara@MyMacs-MacBook-Pro.local>
2020-06-22 22:34:50 -0500

    got product for edit prod, functionality needs to be done

--- client/components/AlchemEditProduct.js
+++ client/components/AlchemEditProduct.js
@@ -3,9 +3,16 @@ import React from 'react'
 export default class AlchemEditProduct extends React.Component {
   constructor() {
     super()
+    this.state = {
+      name: '',
+      picture: '',
+      price: 0,
+      description: ''
+    }
   }
   componentDidMount() {}
   render() {
+    const product = this.props.location.state
     return (
       <section id="editProdSection">
         <div>
@@ -13,11 +20,11 @@ export default class AlchemEditProduct extends React.Component {
           <a id="currentProd">
             <img src="./imagesViews/pratiksha-mohanty-V0xp-dTS3z0-unsplash.jpg" />
             <h3>Product Name: </h3>
-            <p>NAME</p>
+            <p>{product.name}</p>
             <h3>Stock QTY: </h3>
             <p>QTY</p>
             <h3>Product Description:</h3>
-            <p>DESCRIPTION</p>
+            <p>{product.description}</p>
           </a>
           <form id="editProdForm">
             <h2>UPDATE PRODUCT</h2>
